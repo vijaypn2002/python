29fa940fbc1f4f3d5ad9b633e9e84ec50f171b28
vijay <pnvijay250@gmail.com>
2023-09-20 15:54:36 +0530

    new

---
+++ newfile.py
@@ -0,0 +1,97 @@
+# http://127.0.0.1:5500/newwork/newwork.html
+
+#a=int(input("enter any number"))
+#for i in range(6):
+# print('*'*i)
+
+# i=[5,4,3,6,7,3,2,1]
+# for j in i:
+#  print('*'*j)
+
+ #create a python program  to varify the users birth year,birth year above 2002 print 20s kids,below 2000 print 90 kids 
+
+# a=int(input ("enter birth year"))
+# if a==2002:8
+#     print('20s kid')
+# elif a==2000:
+#     print('90s kids')
+# else:
+#     print('none of 20s and 90s')
+
+
+#create a python program to recieve user password,password there should be number symbol alphabets
+
+# a=str(input('enter your password'))
+# if len(a)>=8:
+#     for i in a:
+#         if a.isdigit():
+         
+#         if a.isalnum():
+
+
+#  create a python program that takes neamuarical grades as input and output the corresponding grade in letters (a,b,c,d,f)90-100a,60-65-d, >59 fail
+
+# a=int(input("enter your persantage"))
+# if a>=90:
+#   print('a grade')
+# elif a>=80:
+#   print('b grade')  
+# elif a>=70:
+#   print('c grade') 
+# elif a>=60:
+#   print('d grade') 
+# else:
+#   print('failed')
+
+# write a program to check the triangle is equilatral triangle,isosulas triangle,or skalene
+
+# a=int(input(" first side of triangle"))
+# b=int(input(" second side of triangle"))
+# c=int(input(" third side of triangle"))
+# if a==b==c:
+#   print('equilatral triangle')
+# elif a!=b!=c:
+#   print('scalene')
+# else:
+#   print('issosulor')
+ 
+
+
+# a=str('vijay')
+# print(a,'before')
+# for i in a:
+#  value=["v","i","j","a","y"]
+#  value[1]
+# print(value [-2])
+# value[3]
+# print(value [1])
+
+
+#check weather paligndrome or not
+# a=str(input('enter a string'))
+# if a==a[::-1]:
+#     print("palingdrome")
+# else:
+#     print("not palingdrome")
+ 
+
+# a=int(input("enter any number"))
+# for i in range(30,0,-1):
+#  print('*'*i)
+
+
+# a=int(input('enter any number'))
+# for i in range(0,a):
+#    print('*'*i)
+# for i in range(a,0,-1):
+#   print('*'*i)
+
+
+
+k=1
+a=int(input('enter the number'))
+for i in range(0,a):
+ for j in range(i):
+    print(k,end=' ')
+    k=k+1
+ print(' ')
