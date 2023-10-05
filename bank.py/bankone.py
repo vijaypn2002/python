6a1809770198913ba3ef0023b0a1604fa8604fdf
vijay <pnvijay250@gmail.com>
2023-10-05 08:55:10 +0530

    new

--- bank.py/bankone.py
+++ bank.py/bankone.py
@@ -1,19 +1,35 @@
+
 bank=[]
-for i in range(1):
-    thisdict ={
+thisdict ={
         "name":"",
         "branch":"",
-        "balance":0
-    }
-    name=str(input('enter the  name'))
-    branch=str(input('enter the  branch'))
-    thisdict["name"]=name
-    thisdict["branch"]=branch
-    bank.append(thisdict)
-print(bank)
-balance=int(input("balance"))
-thisdict["balance"]=balance
-bank.append(thisdict)
-print(balance)
+        "balance":0,
+          }
+while True:
+    print("""1.Add Account,
+        2.Add Amount
+        3.Withdraw Amount
+        4.Exit""")
+    choise=(input("enter your choise"))
+    if choise=='1':
+        name=str(input('enter the  name'))
+        branch=str(input('enter the  branch'))
+        thisdict["name"]=name
+        thisdict["branch"]=branch
+        bank.append(thisdict.copy())
+        print(bank)
 
- 
+    elif choise=='2':
+        amount=str(input('account_name'))
+        amount=int(input('add your amount'))
+        thisdict["account_name"]=name
+        thisdict["amount"]=amount
+        bank.append(thisdict)
+        print(bank)
+
+    elif choise=='3':
+        withdraw=int(input("enter your withdrawel amount"))
+        thisdict["withdraw"]=withdraw
+        bank.append(thisdict)
+        print(bank)
+    
