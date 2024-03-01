1c179df21ebf033ba8a10a43c8fff1fd01710782
vijay <pnvijay250@gmail.com>
2024-03-01 17:03:56 +0530

    new file

---
+++ rectangle-area/basic.py
@@ -0,0 +1,13 @@
+def calculate_rectangle_area(length, width):
+    return length * width
+
+def main():
+    length = float(input("Enter the length of the rectangle: "))
+    width = float(input("Enter the width of the rectangle: "))
+    
+    area = calculate_rectangle_area(length, width)
+    
+    print("The area of the rectangle is:", area)
+
+if __name__ == "__main__":
+    main()
